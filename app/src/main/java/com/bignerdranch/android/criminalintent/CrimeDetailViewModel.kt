73c1f2afca56c3f8161d776bfb6e3c59a6b5b57d
Kunel <Kahsapov.K.R@yandex.ru>
2023-12-09 10:58:34 +0700

    Решение проблемы с Transformations

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDetailViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDetailViewModel.kt
@@ -3,7 +3,7 @@ package com.bignerdranch.android.criminalintent
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-
+import androidx.lifecycle.switchMap
 
 
 import java.util.UUID
@@ -13,8 +13,7 @@ class CrimeDetailViewModel() : ViewModel() {
         CrimeRepository.get()
     private val crimeIdLiveData =
         MutableLiveData<UUID>()
-    var crimeLiveData: LiveData<Crime?> =
-        Transformations.switchMap(crimeIdLiveData) { crimeId -> crimeRepository.getCrime(crimeId)
+    var crimeLiveData: LiveData<Crime?> = crimeIdLiveData.switchMap { crimeId -> crimeRepository.getCrime(crimeId)
         }
     fun loadCrime(crimeId: UUID) {
         crimeIdLiveData.value = crimeId
